b23bddcc5a4b74924bb5e34dc151a6b7e47ff6c0
David Michel <davidmichel77@gmail.com>
2017-08-07 17:31:39 +0200

    save state with no css, very ugly

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,53 @@
+require 'open-uri'
+require 'json'
+
+class PagesController < ApplicationController
+  def game
+    @grid = generate_grid
+    @attempt = params[:query]
+  end
+
+  def score
+    @attempt = params[:query]
+    @grid = params[:grid]
+    @end_time = Time.now
+    @start_time = DateTime.parse(params[:time])
+    @time_taken = (@end_time - @start_time).round(2)
+    @message = score_and_message(@attempt, @grid, @time_taken)
+  end
+
+  def generate_grid
+    Array.new(9) { ('A'..'Z').to_a.sample }
+  end
+
+  def included?(guess, grid)
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  end
+
+  def compute_score(attempt, time_taken)
+    time_taken > 60.0 ? 0 : attempt.size * (1.0 - time_taken / 60.0)
+  end
+
+  def score_and_message(attempt, grid, time)
+    if included?(attempt.upcase, grid)
+      if english_word?(attempt)
+        score = compute_score(attempt, time)
+        p "#{score.round(2)} points, well done!"
+      else
+        [0, "not an english word"]
+      end
+    else
+      [0, "not in the grid"]
+    end
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+end
+
+
+# result = run_game(attempt, grid, start_time, end_time)
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/game'
+
+  get 'pages/score'
+
+  get 'game', to: 'pages#game'
+  get 'score', to: 'pages#score'
+
+  root to: 'pages#game'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
